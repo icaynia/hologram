dc5c652760eac01655c427b4af34ebcfd454105a
fmowl10 <jinseok1001@hotmail.com>
2017-01-16 19:30:05 +0900

    add thread class for send requests

--- python/Main.py
+++ python/Main.py
@@ -7,7 +7,7 @@ try:
     from PIL import Image
     from PIL import ImageTk #gif 및 jpg bmp 파일을 읽을 수 있다.
 except ImportError as err :
-    messagebox.showerror("모듈 없음", "PIL 모듈을 설치해주세요")
+    messagebox.showerror("error", "pleas, install PIL module")
 
 ip = "http://192.168.123.189:3000/upload"
 
@@ -16,23 +16,23 @@ class HologramImageUpload:
         self.root = tkinter.Tk()
         self.root.title("ImageUploader")
         self.label = tkinter.Label(self.root, text = "", width=50, height = 20)
-        self.label.pack()
+        self.label.pack(side = TOP)
         self.fileOpenButton = tkinter.Button(self.root, text = "UPLoadImage", command = self.OpenFile)
-        self.fileOpenButton.pack(side = tkinter.constants.LEFT)
+        self.fileOpenButton.pack(side = tkinter.constants.LEFT, anchor = SW)
         self.urlLinkInput = tkinter.Entry(self.root)
-        self.urlLinkInput.pack(side = tkinter.constants.LEFT)
+        self.urlLinkInput.pack(side = tkinter.constants.LEFT, anchor = SW)
         self.sendUrlLinkButton = tkinter.Button(self.root, text = "urlLinkSend", command = self.sendUrlLink)
-        self.sendUrlLinkButton.pack(side = tkinter.constants.RIGHT)
+        self.sendUrlLinkButton.pack(side = tkinter.constants.LEFT, anchor = SW)
         self.urlLinkInput.bind("<Key-Return>", self.sendUrlLink)
         self.root.mainloop()
     def OpenFile(self):
-        self.FilePath = filedialog.askopenfilename()
+        self.FilePath = filedialog.askopenfilename(filetypes = [("gif files", ".gif")])
         if(self.FilePath == ""): 
             return 
         try: 
             self.image=Image.open(self.FilePath) 
         except: 
-            messagebox.showerror(u"오류 !","파일 열기에 실패했습니다") 
+            messagebox.showerror("error","failed to open the image") 
         else: 
             self.pixel=self.image.load() 
             self.label.image=ImageTk.PhotoImage(self.image)
--- python/RequestHologram.py
+++ python/RequestHologram.py
@@ -1,7 +1,32 @@
+import threading
 try:
     import requests
 except ImportError as err:
     messagebox.showerror("모듈 없음", "requests 모듈을 설치해주세요")
+
+class SendGIFImage(threading.Thread):                          #sending gif image to server via thread
+    def __init__(self, threadID, name, counter, url, filePath):
+        threading.Thread.__init__(self)
+        self.threadID = threadID 
+        self.name = naem
+        self.counter = counter
+        self.url = url
+        self.filePath = filePath
+    def run(self):
+        image = {'image' : ('image.gif', open(self.filePath, 'rb'), 'image/gif',{'Exprice : 0'})}
+        r = requests.post(self.url, files = image)
+
+class SendURLLink(threading.Thread):                          #sending youtube link to server via thread
+    def __init__(self, threadID, name, counter, url, urlLink):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.url = url
+        self.urlLink = urlLink
+    def run(self):
+        youtubeUrl = {'url':self.urlLink}
+        r = requests.post(self.url, youtubeUrl)
 
 def sendGifImage(url = "", filePath = ""):
     if(url == "" or filePath == ""):
